147320314a836c2a2de37f062080804fb9f8ab18
Dmitry <karpov0910@mail.ru>
2022-05-22 14:12:10 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,7 +2,18 @@ package ru.job4j.condition;
 
 public class Max {
 
-    public static int max(int left, int right) {
-        return left >= right ? left : right;
+    public static int max(int first, int second) {
+        return first >= second ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        int rsl = max(first, second);
+        return third >= rsl ? third : rsl;
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        int rsl = max(first, second);
+        int rsl2 = max(third, fourth);
+        return rsl2 >= rsl ? rsl2 : rsl;
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -30,4 +30,46 @@ public class MaxTest {
         int expected = 50;
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void whenMax50To51To52To53Then53() {
+        int first = 50;
+        int second = 51;
+        int third = 52;
+        int fourth = 53;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 53;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax50To51To50To53Then55() {
+        int first = 50;
+        int second = 51;
+        int third = 50;
+        int fourth = 55;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 55;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax50To501To50Then501() {
+        int first = 50;
+        int second = 501;
+        int third = 50;
+        int result = Max.max(first, second, third);
+        int expected = 501;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax50To5To50Then50() {
+        int first = 50;
+        int second = 5;
+        int third = 50;
+        int result = Max.max(first, second, third);
+        int expected = 50;
+        Assert.assertEquals(result, expected);
+    }
 }
