bf922f5b4de2ac352ea3e331d94dec11c153b944
Dmitry <karpov0910@mail.ru>
2022-05-22 14:44:18 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -4,15 +4,32 @@ public class Point {
 
     private int x;
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point that) {
         double rsl = Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2));
         return rsl;
+    }
+
+    public double distance3d(Point that) {
+        double rsl = Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2)
+                + Math.pow((this.z - that.z), 2));
+        return rsl;
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -41,4 +41,22 @@ public class PointTest {
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void when1916to2812then4dot24() {
+        double expected = 4.24;
+        Point a = new Point(1, 9, 16);
+        Point b = new Point(2, 8, 12);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when1613to249then4dot58() {
+        double expected = 4.58;
+        Point a = new Point(1, 6, 13);
+        Point b = new Point(2, 4, 9);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
 }
